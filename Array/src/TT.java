9bf7bd0ff32b72c2ec1e32ee688aef2a079e09a2
lyj <814093077@qq.com>
2018-07-28 17:01:03 +0800

    klsjdkfljlkj

---
+++ Array/src/TT.java
@@ -0,0 +1,7 @@
+/**
+ * @author lyj
+ * @date 2018/7/28 - 16:55
+ */
+public class TT {
+
+}
---
+++ Array/src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @author lyj
+ * @date 2018/7/28 - 16:45
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int i=8;
+
+        System.out.printf(" %d\n %d\n %d\n ",i,++i,--i);
+
+    }
+}
